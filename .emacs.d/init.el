0f02118daab4261f0a3a4d3acaaeb1c976a66fb1
Michael Smith <msmith@msmith.id.au>
2010-12-26 15:18:33 +1100

    Set up viper+vimpulse.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,12 @@
+; Remove ui elements.
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+(add-to-list 'load-path "~/.emacs.d/site-lisp")
+
+; Enable vimpulse and surround.
+(add-to-list 'load-path "~/.emacs.d/site-lisp/vimpulse")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/vimpulse-surround")
+(require 'vimpulse)
+(require 'vimpulse-surround)
